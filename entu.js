727d88093fd8b9f0691b89fe72f1a974dc073396
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-10-26 15:02:55 +0200

    dev

---
+++ entu.js
@@ -0,0 +1,295 @@
+var path     = require('path')
+var debug    = require('debug')('app:' + path.basename(__filename).replace('.js', ''))
+var request  = require('request')
+var async    = require('async')
+var op       = require('object-path')
+var md       = require('marked')
+var random   = require('randomstring')
+var crypto   = require('crypto')
+// var sanitize = require('sanitize-html')
+
+
+LIMIT_PARALLEL = 10
+
+function sign_data(data) {
+    data = data || {}
+
+    if (!APP_ENTU_USER || !APP_ENTU_KEY) return data
+
+    var conditions = []
+    for (k in data) {
+        conditions.push({k: data[k]})
+    }
+
+    var expiration = new Date()
+    expiration.setMinutes(expiration.getMinutes() + 10)
+
+    data.user = APP_ENTU_USER
+    var buff_str = JSON.stringify({expiration: expiration.toISOString(), conditions: conditions})
+    data.policy = new Buffer(buff_str).toString('base64')
+    data.signature = crypto.createHmac('sha1', APP_ENTU_KEY).update(data.policy).digest('base64')
+
+    return data
+}
+
+
+//Get entity from Entu
+var get_entity = function get_entity(id, auth_id, auth_token, callback) {
+    if (auth_id && auth_token) {
+        var headers = {'X-Auth-UserId': auth_id, 'X-Auth-Token': auth_token}
+        var qs = {}
+    } else {
+        var headers = {}
+        var qs = sign_data()
+    }
+
+    // debug('get_entity: ' + APP_ENTU_URL + '/entity-' + id)
+    request.get({url: APP_ENTU_URL + '/entity-' + id, headers: headers, qs: qs, strictSSL: true, json: true}, function(error, response, body) {
+        if (error) {
+            return callback(error)
+        }
+        if (response.statusCode !== 200 || !body.result) return callback(new Error(op.get(body, 'error', body)))
+        var properties = op.get(body, 'result.properties', {})
+        var entity = {
+            id: op.get(body, 'result.id', null),
+            displayname: op.get(body, 'result.displayname', null),
+            displayinfo: op.get(body, 'result.displayinfo', null),
+            definition: op.get(body, 'result.definition.keyname', null),
+            picture: APP_ENTU_URL + '/entity-' + op.get(body, 'result.id', null) + '/picture',
+            right: op.get(body, 'result.right', null),
+            properties: {}
+        }
+        for (var p in properties) {
+            if (op.has(properties, [p, 'values'])) {
+                for (var v in op.get(properties, [p, 'values'])) {
+                    if (op.get(properties, [p, 'datatype']) === 'file') {
+                        op.push(entity, ['properties', p], {
+                            id: op.get(properties, [p, 'values', v, 'id']),
+                            value: op.get(properties, [p, 'values', v, 'value']),
+                            file: APP_ENTU_URL + '/file-' + op.get(properties, [p, 'values', v, 'db_value'])
+                        })
+                    } else if (op.get(properties, [p, 'datatype']) === 'text') {
+                        op.push(entity, ['properties', p], {
+                            id: op.get(properties, [p, 'values', v, 'id']),
+                            value: op.get(properties, [p, 'values', v, 'value']),
+                            md: md(op.get(properties, [p, 'values', v, 'db_value']))
+                        })
+                    } else if (op.get(properties, [p, 'datatype']) === 'reference') {
+                        op.push(entity, ['properties', p], {
+                            id: op.get(properties, [p, 'values', v, 'id']),
+                            value: op.get(properties, [p, 'values', v, 'value']),
+                            reference: op.get(properties, [p, 'values', v, 'db_value'])
+                        })
+                    } else {
+                        op.push(entity, ['properties', p], {
+                            id: op.get(properties, [p, 'values', v, 'id']),
+                            value: op.get(properties, [p, 'values', v, 'value']),
+                        })
+                    }
+                }
+                if (op.get(properties, [p, 'multiplicity']) === 1) op.set(entity, ['properties', p], op.get(entity, ['properties', p, 0]))
+            }
+        }
+        // debug(JSON.stringify(entity, null, '  '))
+        callback(null, op(entity))
+    })
+}
+
+
+
+//Get entities by definition
+var get_entities = function get_entities(definition, limit, auth_id, auth_token, callback) {
+    if (!definition) {
+        return callback(new Error('Missing "definition"'))
+    }
+
+    var qs = {definition: definition}
+    if (limit) {
+        qs['limit'] = limit
+    }
+    if (auth_id && auth_token) {
+        var headers = {'X-Auth-UserId': auth_id, 'X-Auth-Token': auth_token}
+    } else {
+        var headers = {}
+        qs = sign_data(qs)
+    }
+
+    var url = '/entity'
+    var loop = 'result'
+
+    debug('get_entities: ' + APP_ENTU_URL + url)
+    request.get({url: APP_ENTU_URL + url, headers: headers, qs: qs, strictSSL: true, json: true}, function(error, response, body) {
+        if (error) {
+            return callback(error)
+        }
+        if (response.statusCode !== 200 || !body.result) {
+            return callback(new Error(op.get(body, 'error', body)))
+        }
+
+        var entities = []
+        async.eachLimit(op.get(body, loop, []), LIMIT_PARALLEL, function(e, callback) {
+            get_entity(e.id, auth_id, auth_token, function(error, op_entity) {
+                if (error) {
+                    return callback(error)
+                }
+                entities.push(op_entity)
+                callback()
+            })
+        }, function(error) {
+            if (error) {
+                return callback(error)
+            }
+            // debug(definition + ' returned ' +  entities.length + ' entities.')
+            callback(null, entities)
+        })
+    })
+}
+
+
+//Get childs by parent entity id and optionally by definition
+var get_childs = function get_childs(parent_entity_id, definition, auth_id, auth_token, callback) {
+    if (!parent_entity_id) {
+        callback(new Error('Missing "parent_entity_id"'))
+        return
+    }
+    // debug('get childs for ' + parent_entity_id)
+    var qs = {}
+    if (definition) {
+        qs = {definition: definition}
+    }
+
+    if (auth_id && auth_token) {
+        var headers = {'X-Auth-UserId': auth_id, 'X-Auth-Token': auth_token}
+    } else {
+        var headers = {}
+        qs = sign_data(qs)
+    }
+    var url = '/entity-' + parent_entity_id + '/childs'
+    // debug('get_childs: ' + APP_ENTU_URL + url)
+    var options = {
+        url: APP_ENTU_URL + url,
+        headers: headers,
+        qs: qs,
+        strictSSL: true,
+        json: true
+    }
+    request.get(options, function(error, response, body) {
+        if (error) {
+            callback(error)
+            return
+        }
+        if (response.statusCode !== 200 || !body.result) {
+            callback(new Error(op.get(body, 'error', body)))
+            return
+        }
+
+        var definitions = Object.keys(body.result)
+        var childs = []
+        async.eachLimit(
+            definitions,
+            LIMIT_PARALLEL,
+            function doLoop(definition, doLoopCB) {
+                var loop = ['result', definition, 'entities']
+                async.each(op.get(body, loop, []), function(e, eachCB) {
+                    get_entity(e.id, auth_id, auth_token, function(error, child_e) {
+                        if (error) {
+                            eachCB(error)
+                            return
+                        }
+                        child_e.set('_display', {name: e.name, info: e.info})
+                        childs.push(child_e)
+                        eachCB()
+                    })
+                }, function gotByDef(error) {
+                    if (error) {
+                        doLoopCB(error)
+                        return
+                    }
+                    doLoopCB(null)
+                })
+            },
+            function endLoop(error) {
+                if (error) {
+                    callback(error)
+                    return
+                }
+                // debug(parent_entity_id + ' has ' +  childs.length + ' childs.')
+                callback(null, childs)
+            }
+        )
+    })
+}
+
+
+//Add entity
+var add = function add(parent_entity_id, definition, properties, auth_id, auth_token, callback) {
+    var data = {
+        definition: definition
+    }
+
+    for (p in properties) {
+        data[definition + '-' + p] = properties[p]
+    }
+
+    if (auth_id && auth_token) {
+        var headers = {'X-Auth-UserId': auth_id, 'X-Auth-Token': auth_token}
+        var qb = data
+    } else {
+        var headers = {}
+        var qb = sign_data(data)
+    }
+
+    var options = {
+        url: APP_ENTU_URL + '/entity-' + parent_entity_id,
+        headers: headers,
+        body: qb,
+        strictSSL: true,
+        json: true
+    }
+    // debug(JSON.stringify(options, null, 4))
+    request.post(options, function(error, response, body) {
+        if (error) {
+            debug('E:', error)
+            callback(error)
+            return
+        }
+        if (response.statusCode !== 201 || !body.result) {
+            debug('E:' + response.statusCode, body)
+            callback(new Error(op.get(body, 'error', body)))
+            return
+        }
+        callback(null, op.get(body, 'result.id', null))
+    })
+}
+
+
+
+//Share entity
+var rights = function rights(id, person_id, right, auth_id, auth_token, callback) {
+    var body = {
+        entity: person_id,
+        right: right
+    }
+    if(auth_id && auth_token) {
+        var headers = {'X-Auth-UserId': auth_id, 'X-Auth-Token': auth_token}
+        var qb = body
+    } else {
+        var headers = {}
+        var qb = sign_data(body)
+    }
+
+    request.post({url: APP_ENTU_URL + '/entity-' + id + '/rights', headers: headers, body: qb, strictSSL: true, json: true}, function(error, response, body) {
+        if(error) return callback(error)
+        if(response.statusCode !== 200) return callback(new Error(op.get(body, 'error', body)))
+
+        callback(null, id)
+    })
+}
+
+
+
+exports.get_entity      = get_entity
+exports.get_childs      = get_childs
+exports.get_entities    = get_entities
+exports.add             = add
+exports.rights          = rights
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+
+
 if(process.env.NEW_RELIC_LICENSE_KEY) require('newrelic')
 
+var path            = require('path')
+var debug           = require('debug')('app:' + path.basename(__filename).replace('.js', ''))
 var request         = require('request')
 var fs              = require('fs')
 var path            = require('path')
@@ -7,219 +11,59 @@ var Transform       = require('stream').Transform
 
 var gm              = require('gm')
 
-var entulib         = require('./entulib.js')
+// var entulib         = require('./entulib.js')
+var entu            = require('./entu')
 var queue           = require('./queue.js')
 var helper          = require('./helper.js')
+
+var async           = require('async')
 
 
 
 var pjson = require('./package.json')
 console.log('----==== ' + pjson.name + ' v.' + pjson.version + ' ====----')
 
-PAGE_SIZE_LIMIT = 1000
-QUEUE_SIZE = 3
-FIRST_PAGE = 36
-SLEEPING = false
 HOME_DIR = path.dirname(process.argv[1])
 TEMP_DIR = path.resolve(HOME_DIR, 'temp')
-PIC_READ_ENTITY = 'eksponaat'
+PIC_READ_DEFINITION = ['eksponaat', 'eksemplar']
 PIC_READ_PROPERTY = 'photo-orig'
 PIC_WRITE_PROPERTY = 'photo'
 VALID_EXTENSIONS = ['.jpg', '.pdf', '.tif', '.tiff', '.png', '.jpeg', '.gif']
 
-HOSTNAME = 'okupatsioon.entu.ee'
-ENTU_URI = 'https://' + HOSTNAME + '/'
-ENTU_API = ENTU_URI + 'api2/'
-ENTU_API_ENTITY = ENTU_API + 'entity-'
-ENTU_API_POST_FILE = ENTU_API + 'file/s3'
+APP_ENTU_URL = 'https://okupatsioon.entu.ee/api2'
 
-var q = queue(QUEUE_SIZE)
-var EntuLib = entulib(process.env.PM_ENTITY, process.env.PM_KEY, HOSTNAME)
+APP_ENTU_USER = process.env.ENTU_USER
+APP_ENTU_KEY = process.env.ENTU_KEY
 
 
-var fetchNextPage = function fetchNextPage(page) {
-    EntuLib.findEntity(PIC_READ_ENTITY, '', PAGE_SIZE_LIMIT, page, function findEntityCB(err, result) {
+
+async.eachSeries(PIC_READ_DEFINITION, function(read_definition, callback) {
+    entu.get_entities(read_definition, null, null, null, function(err, op_entities) {
         if (err) {
-            console.log('findEntityCB: Can\'t reach Entu', err, result)
-            setTimeout(function() { fetchNextPage(page) }, 10*1000)
+            debug('Fetch ' + read_definition + ' from Entu failed.', err)
+            callback(err)
+            return
         }
-        else if (result.error !== undefined) {
-            console.log (result, 'Failed to fetch from Entu.')
-            setTimeout(function() { fetchNextPage(page) }, 10*1000)
-        } else {
-            result.result.forEach(function entityLoop(entity) {
-                EntuLib.getEntity(entity.id, function getEntityCB(err, result) {
-                    if (err) {
-                        console.log('getEntityCB: Can\'t reach Entu', err, result)
-                        return
-                    }
-                    else if (result.error !== undefined) {
-                        console.log(result.error, 'Failed to fetch from Entu.')
-                        return
-                    } else {
-                        var photo_property = result.result.properties[PIC_READ_PROPERTY]
-                        var thumb_property = result.result.properties[PIC_WRITE_PROPERTY]
-                        var code_value = result.result.properties['code'].values ? result.result.properties['code'].values[0].value : ''
-                        var nimetus_value = result.result.properties['tag'].values ? result.result.properties['tag'].values[0].value : ''
-
-                        var to_create = {}
-                        var to_delete = {}
-                        var orig_cnt = 0
-                        var thumb_cnt = 0
-                        if (photo_property.values) {
-                            photo_property.values.forEach(function photoLoop(photo_val) {
-                                if (VALID_EXTENSIONS.indexOf(path.extname(photo_val.value).toLowerCase()) === -1) {
-                                    console.log(Date().toString() + ' Ignoring file with unsupported extension (' + path.extname(photo_val.value) + ') on entity ' + entity.id)
-                                    return
-                                }
-                                orig_cnt ++
-                                to_create[photo_val.value] = photo_val
-                            })
-                        }
-                        if (thumb_property.values) {
-                            thumb_property.values.forEach(function thumbLoop(thumb_val) {
-                                if (to_create[thumb_val.value]) {
-                                    delete to_create[thumb_val.value]
-                                    orig_cnt --
-                                } else {
-                                    to_delete[thumb_val.value] = thumb_val
-                                    thumb_cnt ++
-                                }
-                            })
-                        }
-                        if (orig_cnt > 0 || thumb_cnt > 0) {
-                            console.log(Date().toString() + ' Work on eid:' + entity.id, {"to_create":orig_cnt, "to_delete":thumb_cnt})
-                            for (var delete_key in to_delete) {
-                                EntuLib.removeProperty(entity.id, PIC_WRITE_PROPERTY, to_delete[delete_key].id, function() {
-                                    console.log(Date().toString() + ' Removed thumb ' + to_delete[delete_key].id + ' from entity ' + entity.id)
-                                })
-                            }
-                            for (var create_key in to_create) {
-                                var photo_val = to_create[create_key]
-                                var jobData = {
-                                    'eid':          entity.id,
-                                    'photo_db_val': photo_val.db_value,
-                                    'photo_val':    photo_val.value,
-                                    'code_val':     code_value,
-                                    'nimetus_val':  nimetus_value
-                                }
-                                q.add('Processing photo ' + photo_val.db_value, jobData, function jobFunction(jobData, finalCB) {
-                                    // setTimeout(finalCB, 5*1000)
-                                    fetchFile(jobData.eid, jobData.photo_db_val, jobData.photo_val, jobData.code_val, jobData.nimetus_val, finalCB)
-                                })
-                            }
-                        }
-                    }
-                })
-            })
-
-            if (q.stats().active) {
-                console.log(Date().toString() + '=== Active/queued connections: ' + q.stats().active + '/' + q.stats().queue)
+        async.eachSeries(op_entities, function(op_entity, callback) {
+            processEntity(op_entity, callback)
+        }, function(err, op_entities) {
+            if (err) {
+                debug('Processing ' + op_entity.get('id') + ' failed.', err)
+                callback(err)
+                return
             }
-            q.start()
-
-            if (PAGE_SIZE_LIMIT * page < result.count) {
-                var fetchIfReady = function fetchIfReady(page) {
-                    if (Object.keys(q.stats().jobs).length) {
-                        // console.log(Date().toString() + '=== Active jobs: ', q.stats().jobs)
-                    }
-                    if (q.stats().active === 0) {
-                        console.log(Date().toString() + '=== Loading page #' + page + '/' + Math.ceil(result.count/PAGE_SIZE_LIMIT))
-                        fetchNextPage(page)
-                    } else if (q.stats().active < QUEUE_SIZE) {
-                        console.log(Date().toString() + '=== Active/queued connections: ' + q.stats().active + '/' + q.stats().queue + '. Loading next page #' + page + '/' + Math.ceil(result.count/PAGE_SIZE_LIMIT))
-                        setTimeout(function() { fetchNextPage(page) }, 1000)
-                    } else {
-                        // console.log(Date().toString() + '=== Active/queued connections: ' + q.stats().active + '/' + q.stats().queue + '. Postpone next page #' + page + '/' + Math.ceil(result.count/PAGE_SIZE_LIMIT))
-                        setTimeout(function() { fetchIfReady(page) }, 10*1000)
-                    }
-                }
-                fetchIfReady(page + 1)
-            } else {
-                console.log(Date().toString() + '=== New roundtrip in ' + helper.msToTime(1000 * 60 * process.env.PM_NIGHT_MINUTES) + '. Active jobs: ', q.stats().jobs)
-                setTimeout(function() { SLEEPING = false }, 1000 * 60 * process.env.PM_NIGHT_MINUTES)
-                SLEEPING = true
-                setTimeout(function() { fetchNextPage(1) }, 1000 * 60 * process.env.PM_NIGHT_MINUTES)
-            }
-
-        }
-    })
-}
-
-fetchNextPage(FIRST_PAGE)
-
-
-
-var total_download_size = 0
-var bytes_downloaded = 0
-
-var append_background = path.resolve(HOME_DIR, 'text_background.png')
-
-var fetchFile = function fetchFile(entity_id, file_id, file_name, exp_nr, nimetus, finalCB) {
-    // console.log('fetchFile ', entity_id, file_id, file_name, exp_nr, nimetus)
-    var original_filepath = path.resolve(TEMP_DIR, file_id + '.' + file_name)
-    var original_file_stream = fs.createWriteStream(original_filepath)
-    var converted_filepath = path.resolve(TEMP_DIR, file_id + '.' + 'jpg')
-    var converted_file_stream = fs.createWriteStream(converted_filepath)
-
-    var validatorTransform = new Transform()
-    validatorTransform._transform = function(data, encoding, done) {
-        this.push(data)
-        original_file_stream.write(data)
-        done()
-    }
-
-    gm(EntuLib.getFileStream(file_id).pipe(validatorTransform))
-    .stream('jpg', function(err, stdout, stderr) {
-        gm(stdout)
-        .resize(800, 530)
-        .rotate('#ffffffff', 0)
-        .stream('jpg', function(err, stdout, stderr) {
-            gm(stdout)
-            .background('#ffffff')
-            .append(append_background)
-            .stream(function(err, stdout, stderr) {
-                gm(stdout)
-                .drawText(0, 15, 'Okupatsioonide Muuseum #' + exp_nr + '\n' + nimetus + '\nokupatsioon.entu.ee', 'south')
-                .stream(function(err, stdout, stderr) {
-                    stdout.pipe(converted_file_stream)
-                    stdout.on('end', function() {
-                        // setTimeout(finalCB, 10*1000); return; // For dry-run purposes
-                        EntuLib.addFile(entity_id, PIC_READ_ENTITY + '-' + PIC_WRITE_PROPERTY, file_name, 'image/jpeg', converted_file_stream.bytesWritten, converted_filepath, function addFileCB(err, result) {
-                            if (err) {
-                                console.log(Date().toString() + ' SKIPPING OVER: addFileCB: ' + file_name + ' ' + converted_filepath, err, result)
-                                return
-                            }
-                            console.log(Date().toString() + ' Finished upload of ' + original_filepath + ' ' + helper.bytesToSize(converted_file_stream.bytesWritten) + '.')
-                            fs.unlink(original_filepath)
-                            fs.unlink(converted_filepath)
-                        })
-                        finalCB()
-                    })
-                })
-            })
+            callback()
         })
     })
-}
-
-
-var pulse_cnt = 0
-var pulse_ms = 60*1000
-var hibernation_factor = 20
-var pulse = function pulse() {
-    if (SLEEPING) {
-        console.log(Date().toString() + ' ...zzzZZ (' + helper.msToTime(pulse_cnt * pulse_ms) + ')')
-        pulse_cnt += hibernation_factor
-        setTimeout(function() { pulse() }, pulse_ms * hibernation_factor)
-    } else {
-        console.log(Date().toString() + ' awake. Lifetime ' + helper.msToTime(pulse_cnt * pulse_ms))
-        pulse_cnt ++
-        setTimeout(function() { pulse() }, pulse_ms)
+}, function(err, op_entities) {
+    if (err) {
+        debug(err)
+        // callback(err)
+        return
     }
+    // callback()
+})
+
+var processEntity = function processEntity(op_entity, callback) {
+    debug(JSON.stringify([op_entity.get('id'), op_entity.get('definition')], null, 2))
 }
-pulse()
-
-
-
-/*
-*/
